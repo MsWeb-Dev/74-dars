93833351a3708b98d6dff03b6fcd6d35a8439c37
SHAXNOZA_DEV <mswebdev175@gmail.com>
2023-05-16 14:22:42 +0500

    74dars yuklanyapti

---
+++ uyga vazifa/app.js
@@ -0,0 +1,13 @@
+
+import {num} from "./script.js"
+const newArr =[...num]
+
+for(let item of newArr){
+    console.log(item)
+    const misol =function(){
+        let a=prompt("A sonini kiriting")
+        let b= prompt("B sonini kiriying")
+    
+    }
+}
+
